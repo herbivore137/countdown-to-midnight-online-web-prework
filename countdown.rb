4c4a7689e0f0b94b63dba778d354b30633902ec8
B.A. Hanlon <herbivore137@gmail.com>
2019-04-01 19:52:13 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,7 +5,7 @@ def countdown(countdown_start)
   number = countdown_start
   
   while number >= 0 
-    puts "{#number} SECOND(S)!"
+    puts "#{number} SECOND(S)!"
     number -= 1 
   end
   "HAPPY NEW YEAR!"
