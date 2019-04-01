3b58f7b9917494ce408c0e2f5447431976587b34
B.A. Hanlon <herbivore137@gmail.com>
2019-04-01 19:48:06 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
 
 def countdown(countdown_input)
+  
+  counter = countdown_input
+  
+  while counter > 0 
+    puts "{#number} SECOND(S)!"
+    counter -= 1 
+  end
+  "HAPPY NEW YEAR!"
 end
